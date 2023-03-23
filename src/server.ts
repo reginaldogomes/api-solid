e2a837128402c3fe2a9571ebce40a90018970928
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-03-23 14:13:00 -0300

    env and dotenv configured

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import { env } from './env'
 import { app } from './app'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Runnig!')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Runnig!')
+  })
